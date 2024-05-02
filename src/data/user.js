195a8b4fd12ed17d3097557645e9724d95ec5b80
pugalkmc <pugalkmc@gmail.com>
2024-05-02 09:45:06 +0530

    My commit

--- src/data/user.js
+++ src/data/user.js
@@ -1,8 +1,8 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		title: "Reactfolio by Sakthivel",
+		name: "Sakthivel Murugan N.",
+		email: "sakthi123@gmail.com",
 		logo: "../logo.png",
 	},
 
@@ -16,13 +16,13 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "And I’m a Design, Testing.",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am currently pursuing a B.E. in Electronics and Communication Engineering at Karpagam Institute of Technology. UI Design, Selenium Testing.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Sakthivel Murugan S. I live in India, where I design the future.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
@@ -35,30 +35,30 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "Sonic-Safe-ANC-Industrial-Solution-Project",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Professional Noise Guard: Cutting-edge ANC tech and wireless communication reduce industrial noise, enhancing worker safety and productivity. Customizable interface ensures seamless integration, offering a tailored solution for a quieter, more efficient workplace.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/SSAKTHIVELMURUGAN/Sonic-Safe-ANC-Industrial-Solution---Project-Report",
 		},
 
 		{
-			title: "Project 2",
+			title: "Practice-Page-Web-AutomationPublic",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Selenium automation scripts for PracticePage website, powered by Selenium WebDriver and TestNG framework. Ensuring comprehensive test coverage and detailed reports, streamlining testing for enhanced efficiency and reliability.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/SSAKTHIVELMURUGAN/Sonic-Safe-ANC-Industrial-Solution---Project-Report",
 		},
 
 		{
-			title: "Project 3",
+			title: "BlogVista",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"BlogVista, an intuitive blog reading app, enhances your experience with a sleek interface. Explore blogs effortlessly, save favorites, and customize preferences for an elevated reading journey.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/SSAKTHIVELMURUGAN/Sonic-Safe-ANC-Industrial-Solution---Project-Report",
 		},
 
 		{
